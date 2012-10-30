b0f397d264ca7f24777122811401eb698cafc4f5
Kieran Darcy <kieranfdarcy@gmail.com>
2012-10-30 16:08:45 +0000

    Step 3 - Create some database models

--- app.py
+++ app.py
@@ -1,6 +1,37 @@
+import uuid
+
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Models
+qrcodes = db.Table('qrcodes',
+    db.Column('qrcode_id', db.Integer, db.ForeignKey('qrcode.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Unicode, unique=True)
+    name = db.Column(db.Unicode)
+    qrcodes = db.relationship('Qrcode', secondary=qrcodes,
+        backref=db.backref('users', lazy='dynamic'))
+
+    def __init__(self, username, name):
+        self.username = username
+        self.name = name
+
+class Qrcode(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Unicode, unique=True)
+    filename = db.Column(db.Unicode, unique=True)
+    #users = an implied column
+    
+    def __init__(self, content):
+        self.content = content
+        self.filename = os.path.join(app.instance_path, u'%s.png' % uuid.uuid4())
+
 
 @app.route('/')
 def home():
